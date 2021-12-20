47b1222f27960771a936d0282df7253104608ec8
sungmuk Kang <miranaky@gmail.com>
2021-12-20 22:10:35 +0900

    FEAT: Movie Review Detail
    - 영화 리뷰에 영화 리뷰 추천 갯수를 표시하도록 변경
    - 영화 리뷰 디테일 API와 영화 디테일 API 리뷰 부분에서 확인 가능

--- reviews/models.py
+++ reviews/models.py
@@ -4,23 +4,24 @@ from django.db import models
 
 class Review(models.Model):
     text = models.TextField()
-    rating = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(10)]
-    )
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    movie_id = models.ForeignKey(
-        "movies.Movie", related_name="reviews", on_delete=models.CASCADE
-    )
+    movie_id = models.ForeignKey("movies.Movie", related_name="reviews", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.movie_id}-{self.text}"
 
+    @property
+    def vote(self):
+        vote = self.review_vote.count()
+        if vote is None:
+            return 0
+        return vote
+
 
 class ReviewVote(models.Model):
-    review_id = models.ForeignKey(
-        "Review", related_name="review_vote", on_delete=models.CASCADE
-    )
+    review_id = models.ForeignKey("Review", related_name="review_vote", on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.review_id.id)
