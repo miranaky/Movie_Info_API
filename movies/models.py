53103f9c6a4f7afcf799130e6a681756d5c24440
sungmuk Kang <miranaky@gmail.com>
2021-12-20 13:36:12 +0900

    Movie & Review model 생성
    - 요구사항에 맞도록 Movie,Review 모델 생성
    - movie : id,title,year,rating,genres,summary
    - review : id, movie_id, text, created_at, updated_at

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=80, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+
+    title = models.CharField(max_length=180)
+    year = models.CharField(max_length=4)
+    summary = models.TextField()
+    genres = models.ManyToManyField("Genre", related_name="movie", blank=True)
+
+    def rating(self) -> int:
+        rating = self.reviews.all().aggregate(models.Avg("rating")).get("rating__avg")
+        if rating is None:
+            rating = 0
+        return rating
+
+    def __str__(self):
+        return self.title
---
+++ reviews/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Review(models.Model):
+    text = models.TextField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    movie = models.ForeignKey("movies.Movie", related_name="reviews", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.movie}-{self.text}"
